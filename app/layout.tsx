3d37126d329e943bc608bdc6429afc83b33ef3c7
Nicolás Zagal Vallejo <u20201c429@upc.edu.pe>
2026-09-17 00:15:46 -0500

    fix: Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 const GTM_ID = "GTM-M5XQKM4R";
+const GA_ID = "G-GM7H19L9F7";
 
 export default function RootLayout({
                                        children,
@@ -23,6 +24,20 @@ export default function RootLayout({
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','${GTM_ID}');`,
+                }}
+            />
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script
+                id="ga-gtag"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_ID}');`,
                 }}
             />
         </head>
